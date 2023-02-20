453155be4aad2096b0bee50694edf5c1c750c12e
Shubham Singh <53564021+ShubhamSingh03@users.noreply.github.com>
2023-02-20 21:51:39 +0530

    homePage --layout added

---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,33 @@
+import "./homePage.css";
+
+const HomePage = () => {
+  return (
+    <>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="page-content">
+              <div class="main-banner">
+                <div class="row">
+                  <div class="col-lg-7">
+                    <div class="header-text">
+                      <h6>MADE BY SHUBHAM SINGH</h6>
+                      <h4>
+                        <em>INDIA</em> KA APNA BATTLEGROUNDS
+                      </h4>
+                      <div class="main-button">
+                        <a href="/browse">Browse Now</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
