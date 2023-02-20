18ee178a64fa43963c4bd311e0c85afeb3530612
Shubham Singh <53564021+ShubhamSingh03@users.noreply.github.com>
2023-02-20 21:50:55 +0530

    footer component added

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,23 @@
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              <p>
+                Copyright © 2023 Built by:
+                <a href="https://shubhambhoj.in">Shubham Singh</a>. All rights
+                reserved.
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
