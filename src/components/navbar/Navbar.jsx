0746bffa8dd576c18c607de90524f1a74d44bfe3
Shubham Singh <53564021+ShubhamSingh03@users.noreply.github.com>
2023-02-20 21:50:38 +0530

    Navbar component added

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,60 @@
+import "./navbar.css";
+import logo from "../../assets/logo.png";
+import { Link, NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      console.log("Do nothing Eat 5star");
+    }
+  };
+
+  return (
+    <>
+      <header className="header-area header-sticky">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <nav className="main-nav">
+                <Link to="/" className="logo">
+                  <img src={logo} alt="logo" />
+                </Link>
+
+                <div className="search-input">
+                  <form id="search" action="#">
+                    <input
+                      type="text"
+                      placeholder="Type Something..."
+                      id="searchText"
+                      name="searchKeyword"
+                      onKeyDown={handleKeyPress}
+                    />
+                    <i className="fa fa-search"></i>
+                  </form>
+                </div>
+
+                <ul className="nav">
+                  <li>
+                    <NavLink to={"/"}>Home</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/news">News</NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/about">About Us</NavLink>
+                  </li>
+                </ul>
+                <p className="menu-trigger">
+                  <span>Menu</span>
+                </p>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
