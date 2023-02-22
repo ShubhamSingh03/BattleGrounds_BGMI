db2bbe348a6695457bd535333f2ccec5b1b2b855
Shubham Singh <53564021+ShubhamSingh03@users.noreply.github.com>
2023-02-22 22:12:27 +0530

    footer --layout changed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,16 +1,47 @@
+import { NavLink } from "react-router-dom";
 import "./footer.css";
+import footerLogo from "../../assets/main-logo-images/footerLogo.png";
+
+/**********************************************
+ *
+ * Footer component
+ *
+ **********************************************/
 
 const Footer = () => {
   return (
     <>
       <footer>
         <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-md-12 text-center">
+              <img
+                src={footerLogo}
+                className="footer-logo-img"
+                alt="footer-logo"
+              />
+              <p class="menu">
+                <NavLink to={"/"}> Home</NavLink>
+                <NavLink to={"/news"}> News </NavLink>
+                <NavLink> Esports </NavLink>
+                <NavLink> Streamers </NavLink>
+                <NavLink to={"/about"}>About us</NavLink>
+              </p>
+            </div>
+          </div>
           <div class="row">
-            <div class="col-lg-12">
-              <p>
-                Copyright © 2023 Built by:
-                <a href="https://shubhambhoj.in">Shubham Singh</a>. All rights
-                reserved.
+            <div class="col-md-12 text-center">
+              <p class="copyright">
+                Copyright &copy; 2023 All rights reserved | Coded with{" "}
+                <i class="fa fa-heart" aria-hidden="true"></i> by:
+                <a
+                  className="footer-author-name"
+                  href="https://shubhambhoj.in"
+                  target={"_blank"}
+                  rel="noreferrer"
+                >
+                  Shubham Singh{" "}
+                </a>
               </p>
             </div>
           </div>
