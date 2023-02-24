9dba8c3229ad682feb0a039020c23cbc2c75d31b
Shubham Singh <53564021+ShubhamSingh03@users.noreply.github.com>
2023-02-24 21:57:29 +0530

    helmet synced

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import { BrowserRouter as Router } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <HelmetProvider>
+      <Router>
+        <App />
+      </Router>
+    </HelmetProvider>
   </React.StrictMode>
 );
