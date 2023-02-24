85d4474018b366a7125c9dafeb5a681470455a7d
Shubham Singh <53564021+ShubhamSingh03@users.noreply.github.com>
2023-02-24 21:57:50 +0530

    routes added to App component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import PageLayout from "./layout/PageLayout";
 
 //pages
 import HomePage from "./pages/home/HomePage";
+import AboutusPage from "./pages/aboutus/AboutUsPage";
+import EsportsPage from "./pages/esports/EsportsPage";
 
 const App = () => {
   return (
@@ -15,6 +17,8 @@ const App = () => {
       <Routes>
         <Route element={<PageLayout />}>
           <Route index element={<HomePage />} />
+          <Route path="/esports" element={<EsportsPage />} />
+          <Route path="/about" element={<AboutusPage />} />
         </Route>
       </Routes>
     </>
