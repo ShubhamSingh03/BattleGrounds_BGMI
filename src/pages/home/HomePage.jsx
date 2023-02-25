3d83846aaba6d2b040f48f31baebb31471c8b6cd
Shubham Singh <53564021+ShubhamSingh03@users.noreply.github.com>
2023-02-25 22:25:59 +0530

    GamingDetails component added layout

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,6 +1,20 @@
+//css
 import "./homePage.css";
 
+// imports
+import { Link, useLocation } from "react-router-dom";
+import { gameDetailsData } from "../../config/gameDetailsData";
+
+// components
+import GameDetailsItem from "../../components/gameDetailsItem/GameDetailsItem";
+
 const HomePage = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <>
       <div className="container">
@@ -21,6 +35,39 @@ const HomePage = () => {
                     </div>
                   </div>
                 </div>
+              </div>
+
+              {/* Gaming Library component here */}
+
+              <div className="gaming-library">
+                <div className="col-lg-12">
+                  <div className="heading-section">
+                    <h4>
+                      <em>Other Games</em> Available
+                    </h4>
+                  </div>
+                  {gameDetailsData?.slice(0, 4)?.map((game, index) => (
+                    <GameDetailsItem
+                      key={index}
+                      gameImage={game.gameImage}
+                      gameName={game.gameName}
+                      gameCountry={game.gameCountry}
+                      launchDate={game.launchDate}
+                      totalDownloads={game.totalDownloads}
+                      publisher={game.publisher}
+                      downloadLink={game.downloadLink}
+                    />
+                  ))}
+                </div>
+                {location.pathname === "/" ? (
+                  <div className="col-lg-12">
+                    <div className="main-button">
+                      <Link to="/about">View all Games Available</Link>
+                    </div>
+                  </div>
+                ) : (
+                  ""
+                )}
               </div>
             </div>
           </div>
