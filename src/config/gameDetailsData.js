f7487edbf92e1feef9572fb7d286e175ba3ceae4
Shubham Singh <53564021+ShubhamSingh03@users.noreply.github.com>
2023-02-25 22:24:15 +0530

    gameDetailsData --config added

---
+++ src/config/gameDetailsData.js
@@ -0,0 +1,93 @@
+export const gameDetailsData = [
+  {
+    gameImage: require("../assets/games-logos/pubgLogo.jpg"),
+    gameName: "PUBG MOBILE",
+    gameCountry: "Global version",
+    launchDate: "19 March 2018",
+    totalDownloads: "1.3 Billion +",
+    publisher: "Krafton Inc.",
+    downloadLink:
+      "https://play.google.com/store/apps/details?id=com.tencent.ig&hl=en_IN&gl=US",
+  },
+  {
+    gameImage: require("../assets/games-logos/newStateLogo.png"),
+    gameName: "PUBG: NEW STATE",
+    gameCountry: "Global version",
+    launchDate: "11 Nov 2021",
+    totalDownloads: "50 Million +",
+    publisher: "Krafton Inc.",
+    downloadLink:
+      "https://play.google.com/store/apps/details?id=com.pubg.newstate",
+  },
+  {
+    gameImage: require("../assets/games-logos/bgmiLogo.jpg"),
+    gameName: "BGMI INDIA",
+    gameCountry: "Indian version",
+    launchDate: "2 July 2021",
+    totalDownloads: "100 Million +",
+    publisher: "Krafton Inc.",
+    downloadLink:
+      "https://play.google.com/store/apps/details?id=com.pubg.imobile&gl=US",
+  },
+  {
+    gameImage: require("../assets/games-logos/pubgkrLogo.png"),
+    gameName: "PUBG Mobile KR",
+    gameCountry: "Korean version",
+    launchDate: "14 June 2018",
+    totalDownloads: "20 Million +",
+    publisher: "Krafton Inc.",
+    downloadLink:
+      "https://play.google.com/store/apps/details?id=com.pubg.krmobile",
+  },
+  {
+    gameImage: require("../assets/games-logos/gameForPeaceLogo.webp"),
+    gameName: "GAME FOR PEACE",
+    gameCountry: "Chinese version",
+    launchDate: "7 May 2019",
+    totalDownloads: "250 Million +",
+    publisher: "Tencent Corp.",
+    downloadLink:
+      "https://play.google.com/store/apps/details?id=com.tencent.ig&hl=en_IN&gl=US",
+  },
+
+  {
+    gameImage: require("../assets/games-logos/pubgpcLogo.png"),
+    gameName: "PUBG PC",
+    gameCountry: "Global version",
+    launchDate: "23 March 2017",
+    totalDownloads: "75 million+",
+    publisher: "PUBG Studios & KRAFTON",
+    downloadLink: "https://na.battlegrounds.pubg.com/",
+  },
+  {
+    gameImage: require("../assets/games-logos/pubgmobileLiteLogo.png"),
+    gameName: "PUBG MOBILE LITE",
+    gameCountry: "Global version",
+    launchDate: "10 August 2019",
+    totalDownloads: "100 Million +",
+    publisher: "PROXIMA BETA",
+    downloadLink:
+      "https://play.google.com/store/apps/details?id=com.tencent.iglite&hl=en_IE&gl=US",
+  },
+
+  {
+    gameImage: require("../assets/games-logos/pubgvietnamLogo.jpg"),
+    gameName: "PUBG Mobile VN",
+    gameCountry: "Vietnamese version",
+    launchDate: "11 Jan 2019",
+    totalDownloads: "30 Million +",
+    publisher: "VNG Game Publishing",
+    downloadLink:
+      "https://play.google.com/store/apps/details?id=com.vng.pubgmobile",
+  },
+  {
+    gameImage: require("../assets/games-logos/pubgtaiwanLogo.png"),
+    gameName: "PUBG Mobile TW",
+    gameCountry: "Taiwan version",
+    launchDate: "11 Aug 2018",
+    totalDownloads: "3 Million +",
+    publisher: "HotCool Game",
+    downloadLink:
+      "https://play.google.com/store/apps/details?id=com.rekoo.pubgm",
+  },
+];
