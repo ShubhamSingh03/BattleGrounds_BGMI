dbe64dbf259ed3dcbfbc22d2a5c300ea4082e5c3
Shubham Singh <53564021+ShubhamSingh03@users.noreply.github.com>
2023-02-27 23:54:58 +0530

    loader component added

---
+++ src/components/common/loader/Loader.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect } from "react";
+
+// styles
+import "./loader.css";
+
+// jquery
+import $ from "jquery";
+
+/*************************************
+ *
+ * Loader component on page Load
+ *
+ ************************************/
+
+const Loader = () => {
+  useEffect(() => {
+    // Page loading animation
+    $(window).on("load", function () {
+      $("#js-preloader").addClass("loaded");
+    });
+
+    // Fade out and hide preloader element
+    $("#js-preloader").animate(
+      {
+        opacity: ".8",
+      },
+      800,
+      function () {
+        setTimeout(function () {
+          $("#js-preloader").css("visibility", "hidden").fadeOut();
+        }, 400);
+      }
+    );
+
+    // Parallax effect on .cover element
+    if ($(".cover").length) {
+      $(".cover").parallax({
+        imageSrc: $(".cover").data("image"),
+        zIndex: "1",
+      });
+    }
+  }, []);
+
+  return (
+    <>
+      <div id="js-preloader" className="js-preloader">
+        <div className="preloader-inner">
+          <span className="dot"></span>
+          <div className="dots">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Loader;
