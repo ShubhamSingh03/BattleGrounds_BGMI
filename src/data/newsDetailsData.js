727ed882f2968ba42c0f3952f735b8708eb2fc0f
Shubham Singh <53564021+ShubhamSingh03@users.noreply.github.com>
2023-03-01 17:56:49 +0530

    articles data added to newsComponent

---
+++ src/data/newsDetailsData.js
@@ -0,0 +1,172 @@
+export const newsDetailsData = [
+  {
+    id: "holi-dhamaka-1.9.0-update-patch-notes",
+    newsImageSrc: require("../assets/news-thumbnails/news-banner-02.jpg"),
+    newsTag: "Patch Notes",
+    newsPublishDate: "2023-04-10",
+    newsHeading: "Holi Dhamaka 1.9.0 Update Patch Notes",
+    newsSummary:
+      "The long-awaited March Update is finally here, and we can’t wait to share all the cool stuff we have planned in store for you!",
+    newsInDetail: "Holi Dhamaka 1.9.0 Update Patch Notes.md",
+  },
+  {
+    id: "story-of-the-duo-winners",
+    newsImageSrc: require("../assets/news-thumbnails/news-banner-03.jpg"),
+    newsTag: "Event",
+    newsPublishDate: "2023-03-30",
+    newsHeading: "Story Of The Duo Winners",
+    newsSummary:
+      "The winner list can be viewed in the attached document. Congratulations to all the winners!",
+    newsInDetail: "Story Of The Duo Winners.md",
+  },
+  {
+    id: "server-optimization-maintenance-notice",
+    newsImageSrc: require("../assets/news-thumbnails/news-banner-04.jpg"),
+    newsTag: "Notice",
+    newsPublishDate: "2023-03-25",
+    newsHeading: "Server Optimization Maintenance Notice",
+    newsSummary:
+      "To provide better gaming experience to players, a server optimization maintenance will be carried out.",
+    newsInDetail: "Server Optimization Maintenance Notice.md",
+  },
+  {
+    id: "holi-dhamaka-1.9.0-update-schedule",
+    newsImageSrc: require("../assets/news-thumbnails/news-banner-01.jpg"),
+    newsTag: "Notice",
+    newsPublishDate: "2023-03-10",
+    newsHeading: "Holi Dhamaka 1.9.0 Update Schedule",
+    newsSummary:
+      "We are truly excited to bring the March update to you! Here’s the schedule for distribution of the Holi Dhamaka 1.9.0 Update version on March 18th",
+    newsInDetail: "Holi Dhamaka 1.9.0 Update Schedule.md",
+  },
+  {
+    id: "sanctions-against-cheaters-1-27",
+    newsImageSrc: require("../assets/news-thumbnails/news-banner-05.jpg"),
+    newsTag: "Anti-Cheat",
+    newsPublishDate: "2023-02-19",
+    newsHeading: "Sanctions Against Cheaters (1/27 ~ 2/19)",
+    newsSummary: "47,624 accounts were permanently banned from 1/27 ~ 2/19.",
+    newsInDetail: "Sanctions Against Cheaters.md",
+  },
+  {
+    id: 55,
+    newsImageSrc: require("../assets/news-thumbnails/news-banner-06.jpg"),
+    newsTag: "Event",
+    newsPublishDate: "2023-01-05",
+    newsHeading: "Emotes Everywhere Winners",
+    newsSummary:
+      "The winner list can be viewed in the attached document. Congratulations to all the winners!",
+    newsInDetail: "Emotes Everywhere Winners.md",
+  },
+  {
+    id: "[december-update]-known-issues-updated-Dec-19",
+    newsImageSrc: require("../assets/news-thumbnails/news-banner-07.jpg"),
+    newsTag: "Known Issue",
+    newsPublishDate: "2022-12-21",
+    newsHeading: "[December Update] Known Issues (updated: 12/19, 14:30)",
+    newsSummary:
+      "We would like to inform you about the issues regarding the December Update.",
+    newsInDetail: "[December Update] Known Issues Updated.md",
+  },
+  {
+    id: "privacy-policy-revisions",
+    newsImageSrc: require("../assets/news-thumbnails/news-banner-08.jpg"),
+    newsTag: "Notice",
+    newsPublishDate: "2022-12-11",
+    newsHeading: "Privacy Policy Revisions",
+    newsSummary:
+      "We would like to inform you in prior that some clauses will be revised in our Privacy Policy as of December 14, 2022.",
+    newsInDetail: "Privacy Policy Revisions.md",
+  },
+  {
+    id: "KRAFTON.Inc.-launches-exclusive-christmas-events",
+    newsImageSrc: require("../assets/news-thumbnails/news-banner-09.jpg"),
+    newsTag: "Event",
+    newsPublishDate: "2022-12-01",
+    newsHeading: "KRAFTON, Inc. launches exclusive Christmas events",
+    newsSummary:
+      "Various in-game events and community events celebrating Christmas will be available for players",
+    newsInDetail: "KRAFTON, Inc. launches exclusive Christmas events.md",
+  },
+  {
+    id: "introducing-device-ban",
+    newsImageSrc: require("../assets/news-thumbnails/news-banner-10.jpg"),
+    newsTag: "Anti-Cheat",
+    newsPublishDate: "2022-11-24",
+    newsHeading: "Introducing Device Ban",
+    newsSummary:
+      "To provide fair gameplay and eliminate the use of illegal programs, we would like to announce punishment for cheaters",
+    newsInDetail: "Introducing Device Ban.md",
+  },
+  {
+    id: "we-heard-you",
+    newsImageSrc: require("../assets/news-thumbnails/news-banner-11.jpg"),
+    newsTag: "Notice",
+    newsPublishDate: "2022-10-11",
+    newsHeading: "We Heard You",
+    newsSummary:
+      "We are back with another edition of “We Heard You” where we address your feedback and suggestions!",
+    newsInDetail: "We Heard You.md",
+  },
+  {
+    id: "[september-update]-known-issue",
+    newsImageSrc: require("../assets/news-thumbnails/news-banner-12.jpg"),
+    newsTag: "Known Issue",
+    newsPublishDate: "2022-09-19",
+    newsHeading: "[September Update] Known Issue",
+    newsSummary:
+      "We would like to inform you about the issues regarding the September Update",
+    newsInDetail: "[September Update] Known Issue Updated.md",
+  },
+  {
+    id: "a-positive-update-on-cheaters",
+    newsImageSrc: require("../assets/news-thumbnails/news-banner-13.jpg"),
+    newsTag: "Anti-Cheat",
+    newsPublishDate: "2022-09-05",
+    newsHeading: "A Positive Update on Cheaters",
+    newsSummary:
+      "At the end of July, we promised you that fair gameplay has always been and will be our priority.",
+    newsInDetail: "A Positive Update on Cheaters.md",
+  },
+
+  {
+    id: "pop-a-pataka-winners",
+    newsImageSrc: require("../assets/news-thumbnails/news-banner-14.jpg"),
+    newsTag: "Event",
+    newsPublishDate: "2022-07-25",
+    newsHeading: "Pop A Pataka Winners",
+    newsSummary:
+      "The winner list can be viewed in the attached document. Congratulations to all the winners!",
+    newsInDetail: "Pop A Pataka Winners.md",
+  },
+  {
+    id: "[july-update]-patch-notes",
+    newsImageSrc: require("../assets/news-thumbnails/news-banner-17.png"),
+    newsTag: "Patch Notes",
+    newsPublishDate: "2022-07-15",
+    newsHeading: "[July Update] Patch Notes",
+    newsSummary:
+      "New mode and arena gameplay in the newer version 1.5.0 JULY Update",
+    newsInDetail: "[July Update] Patch Notes.md",
+  },
+  {
+    id: "tag-your-squad-contest-winners",
+    newsImageSrc: require("../assets/news-thumbnails/news-banner-15.jpg"),
+    newsTag: "Event",
+    newsPublishDate: "2022-06-23",
+    newsHeading: "Tag Your Squad Contest Winners",
+    newsSummary:
+      "The winner list can be viewed in the attached document. Congratulations to all the winners!",
+    newsInDetail: "Tag Your Squad Contest Winners.md",
+  },
+  {
+    id: "tag-your-squad",
+    newsImageSrc: require("../assets/news-thumbnails/news-banner-16.jpg"),
+    newsTag: "Event",
+    newsPublishDate: "2022-06-01",
+    newsHeading: "Tag Your Squad",
+    newsSummary:
+      "Tag Your Squad is a unique community event where we release squad stories and all you have to do",
+    newsInDetail: "Tag Your Squad.md",
+  },
+];
