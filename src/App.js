880882795a894cf3ccb6a7396e6c76fe95928291
Shubham Singh <53564021+ShubhamSingh03@users.noreply.github.com>
2023-03-01 17:59:20 +0530

    setting up routes to pages

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import "./App.css";
 // routes
 import { Routes, Route } from "react-router-dom";
 
-//layout
+// layout
 import PageLayout from "./layout/PageLayout";
 
-//pages
+// components
+import BackToTop from "./components/common/BackToTop";
+
+// pages
 import HomePage from "./pages/home/HomePage";
 import AboutusPage from "./pages/aboutus/AboutUsPage";
 import EsportsPage from "./pages/esports/EsportsPage";
+import NewsPage from "./pages/news/NewsPage";
+import NewsInDetailPage from "./pages/news/newsInDetailPage/NewsInDetailPage";
+import StreamerPage from "./pages/streamers/StreamerPage";
+import PageNotFound from "./pages/pageNotFound/PageNotFound";
 
 const App = () => {
   return (
@@ -19,8 +26,13 @@ const App = () => {
           <Route index element={<HomePage />} />
           <Route path="/esports" element={<EsportsPage />} />
           <Route path="/about" element={<AboutusPage />} />
+          <Route exact path="/news" element={<NewsPage />} />
+          <Route path="/news/:id" element={<NewsInDetailPage />} />
+          <Route exact path="/streamers" element={<StreamerPage />} />
         </Route>
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
+      <BackToTop />
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+// styles
+import "./index.css";
+
+// bootstrap import
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+
+// react-router-dom
 import { BrowserRouter as Router } from "react-router-dom";
+
+// react-helmet-async
 import { HelmetProvider } from "react-helmet-async";
-import "./index.css";
+
+// App component
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
