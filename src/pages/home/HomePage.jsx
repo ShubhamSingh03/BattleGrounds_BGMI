aced5de28a6c6c3a93ba93c2878ec02c7107bf36
Shubham Singh <53564021+ShubhamSingh03@users.noreply.github.com>
2023-03-02 17:31:44 +0530

    slider & newsCard added to homepage

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,51 +1,120 @@
-//css
+import React, { useState, useEffect, useMemo } from "react";
+
+// styles
 import "./homePage.css";
 
 // imports
 import { Link, useLocation } from "react-router-dom";
-import { gameDetailsData } from "../../config/gameDetailsData";
+
+// data
+import { gameDetailsData } from "../../data/gameDetailsData";
+import { newsDetailsData } from "../../data/newsDetailsData";
+
+// images & favicons
+import homePageFavicon from "../../assets/favicons/home-page-favicon.ico";
+import homePageBannerImage1 from "../../assets/banner-images/home-page-banner-image-1.jpg";
+import homePageBannerImage2 from "../../assets/banner-images/home-page-banner-image-2.jpg";
+import homePageBannerImage3 from "../../assets/banner-images/home-page-banner-image-3.jpg";
+import homePageBannerImage4 from "../../assets/banner-images/home-page-banner-image-4.jpg";
+import homePageBannerImage5 from "../../assets/banner-images/home-page-banner-image-5.jpg";
+
+// common components
+import HelmetConsumer from "../../components/common/HelmetConsumer";
+import HeadingSection from "../../components/common/HeadingSection";
 
 // components
+import StreamersList from "../../components/homePage/streamers/StreamersList";
+import NewsList from "../../components/newsPage/newsList/NewsList";
 import GameDetailsItem from "../../components/gameDetailsItem/GameDetailsItem";
+
+import Loader from "../../components/common/loader/Loader";
 
 const HomePage = () => {
   const location = useLocation();
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const images = useMemo(
+    () => [
+      homePageBannerImage1,
+      homePageBannerImage4,
+      homePageBannerImage2,
+      homePageBannerImage3,
+      homePageBannerImage5,
+    ],
+    []
+  );
+
+  useEffect(() => {
+    // Load all images in memory to avoid delay and flicker
+    images.forEach((image) => {
+      const img = new Image();
+      img.src = image;
+    });
+
+    // Start slideshow
+    const interval = setInterval(() => {
+      setCurrentSlide((currentSlide) =>
+        currentSlide === images.length - 1 ? 0 : currentSlide + 1
+      );
+    }, 4000);
+    return () => clearInterval(interval);
+  }, [images]);
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
+  const styles = {
+    backgroundImage: `url(${images[currentSlide]})`,
+  };
+
   return (
     <>
+      <HelmetConsumer
+        pageTitle={"Home | Battlegrounds Mobile India"}
+        pageFavicon={homePageFavicon}
+      />
+      <Loader />
       <div className="container">
         <div className="row">
           <div className="col-lg-12">
             <div className="page-content">
-              <div className="main-banner">
+              <div className="main-banner" style={styles}>
                 <div className="row">
                   <div className="col-lg-7">
                     <div className="header-text">
-                      <h6>MADE BY SHUBHAM SINGH</h6>
+                      <h6>Welcome, to the Official Page of</h6>
                       <h4>
-                        <em>INDIA</em> KA APNA BATTLEGROUNDS
+                        <em>INDIA</em> KA APNA <br /> BATTLEGROUNDS
                       </h4>
                       <div className="main-button">
-                        <a href="/browse">Browse Now</a>
+                        <a
+                          href="https://play.google.com/store/apps/details?id=com.pubg.imobile&gl=US"
+                          target={"_blank"}
+                          rel="noreferrer"
+                        >
+                          Download
+                        </a>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
 
+              {/* home page streamers component */}
+              <StreamersList />
+
+              {/* news */}
+              <NewsList newsDetailsData={newsDetailsData} />
+
               {/* Gaming Library component here */}
 
               <div className="gaming-library">
                 <div className="col-lg-12">
-                  <div className="heading-section">
-                    <h4>
-                      <em>Other Games</em> Available
-                    </h4>
-                  </div>
+                  <HeadingSection
+                    firstTitle={"Other Games"}
+                    secondTitle={" Available"}
+                  />
                   {gameDetailsData?.slice(0, 4)?.map((game, index) => (
                     <GameDetailsItem
                       key={index}
@@ -62,7 +131,7 @@ const HomePage = () => {
                 {location.pathname === "/" ? (
                   <div className="col-lg-12">
                     <div className="main-button">
-                      <Link to="/about">View all Games Available</Link>
+                      <Link to="/about">View Games Available</Link>
                     </div>
                   </div>
                 ) : (
